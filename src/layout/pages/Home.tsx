ae0ac8c64cd15ebc0b76f703ea46c8caa5e635bd
vladdt1 <100470279+vladdt1@users.noreply.github.com>
2022-04-18 21:52:25 +0300

    Update Home.tsx

--- src/layout/pages/Home.tsx
+++ src/layout/pages/Home.tsx
@@ -2,9 +2,17 @@ import React, {FC} from 'react'
 
 const Home:FC = () => {
     return(
-        <div>Home
+        <div>
+            <span style={{marginTop:10}}> Всем привет</span>
+            <div style={{marginTop:20}}>
+                <span>Данная игра написанна на React TypeScript при помощи библиотеки MUI</span>
+            </div>
+            <div style={{marginTop:20}}>
+                <span>Автор приложения Студент ЛГТУ Титов Владислав</span>
+            </div>
+            <div style={{marginTop:20}}>
+                Ссылка на GitHab с игрой - <a href='https://github.com/vladdt1/Snake-Game'>Snake-Game</a>
+            </div>
         </div>
     )
 }
-
-export default Home
