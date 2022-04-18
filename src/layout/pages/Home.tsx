018d42221eb348095d36866bab182fbb9edbebb7
vladdt1 <100470279+vladdt1@users.noreply.github.com>
2022-04-18 21:55:15 +0300

    Update Home.tsx

--- src/layout/pages/Home.tsx
+++ src/layout/pages/Home.tsx
@@ -11,7 +11,7 @@ const Home:FC = () => {
                 <span>Автор приложения Студент ЛГТУ Титов Владислав</span>
             </div>
             <div style={{marginTop:20}}>
-                Ссылка на GitHab с игрой - <a href='https://github.com/vladdt1/Snake-Game'>Snake-Game</a>
+                Ссылка на GitHub с игрой - <a href='https://github.com/vladdt1/Snake-Game'>Snake-Game</a>
             </div>
         </div>
     )
